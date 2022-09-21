23f99931ea439b39839527a24d304b141be811da
MLIDDOMAIN1\rahmmos <moshiur_rahman@jhancock.com>
2022-09-20 20:56:18 -0400

    Added TDDPracticeProject project

---
+++ BasicStack.Tests/BasicStackTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BasicStack.Tests
+{
+    [TestClass]
+    public class BasicStackTests
+    {
+       public BasicStackTests()
+        {
+
+        }
+    }
+}
---
+++ FizzBuzz.Tests/FizzBuzzTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz.Tests
+{
+    [TestClass]
+    public class FizzBuzzTests
+    {
+
+        public FizzBuzzTests()
+        {
+            
+        }
+    }
+}
